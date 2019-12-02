7c4507b413ca0082ae28db9851d9267a7bcfaf18
hchiam <hchiam@users.noreply.github.com>
2019-12-02 01:36:05 -0500

    example code in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,34 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const { watch, series, parallel } = require('gulp');
+
+function clean(cb) { cb(); }
+function cssTranspile(cb) { cb(); }
+function cssMinify(cb) { cb(); }
+function jsTranspile(cb) { cb(); }
+function jsBundle(cb) { cb(); }
+function jsMinify(cb) { cb(); }
+function publish(cb) { cb(); }
+
+// watch = watch for file changes ; series = in order ; parallel = maximize concurrency
+exports.build = function() {
+  watch('src/*.js',
+    series(
+      clean,
+      parallel(
+        cssTranspile,
+        series(jsTranspile, jsBundle)
+      ),
+      parallel(cssMinify, jsMinify),
+      publish
+    )
+  );
 }
 
-exports.default = defaultTask
+// function transpile(cb) { cb(); }
+// function minify(cb) { cb(); }
+// function livereload(cb) { cb(); }
+
+// if (process.env.NODE_ENV === 'production') {
+//   exports.build = series(transpile, minify);
+// } else {
+//   exports.build = series(transpile, livereload);
+// }
